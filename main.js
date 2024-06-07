dc25f1f24d54635b84aba09df3130c670b19c01b
Aleksandr Kuraskin <kuraskin0205@gmail.com>
2024-06-07 23:57:47 +0200

    update

--- main.js
+++ main.js
@@ -39,7 +39,39 @@ radioInputDivs.forEach( div => {
 })
 
 function formValidator(item){
-    console.log(item)
-    console.log("Valid I guess...")
-    return false
+    
+    const fnField = document.querySelector(".first-name input")
+    const lnField = document.querySelector(".last-name input")
+    const emailField = document.querySelector(".email input")
+    const queryField = document.querySelectorAll(".radio-block input")
+    const msgField = document.querySelector(".message input")
+    const consentField = document.querySelector(".consent-block input")
+
+    if(fnField.value === ''){
+        
+        let fnSpan = document.querySelector(".first-name span")
+
+        fnSpan.classList.remove('invisible')
+        fnField.classList.add("invalid")
+
+    }
+
+    if(lnField.value === ''){
+
+        let lnSpan = document.querySelector(".last-name span")
+
+        lnSpan.classList.remove('invisible')
+        lnField.classList.add("invalid")
+
+    }
+
+    if(emailField.value === '' || !/^[a-z0-9]{1,}@[a-z]{1,}$/.test(emailField.value)){
+
+        let emailSpan = document.querySelector(".email span")
+
+        emailSpan.classList.remove('invisible')
+        emailField.classList.add("invalid")
+
+    }
+
 }
