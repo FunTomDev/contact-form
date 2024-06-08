af922d9fe03b6ea6b9a41899608399a40a57e7f5
Aleksandr Kuraskin <kuraskin0205@gmail.com>
2024-06-08 14:51:27 +0200

    update

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ function generalRadioChecked(){
     requestRadio.parentElement.classList.remove('radio-div-input-checked')
 
     generalRadio.checked = true
+
+    removeWarning(generalRadio.parentElement.parentElement)
 }
 
 function requestRadioChecked(){
@@ -15,6 +17,8 @@ function requestRadioChecked(){
     requestRadio.parentElement.classList.add('radio-div-input-checked')
 
     requestRadio.checked = true
+
+    removeWarning(requestRadio.parentElement.parentElement)
 }
 
 generalRadio.addEventListener('click', () => {
@@ -38,13 +42,15 @@ radioInputDivs.forEach( div => {
     })
 })
 
-function formValidator(item){
+function formValidator(){
+
+    let isValid = true
     
     const fnField = document.querySelector(".first-name input")
     const lnField = document.querySelector(".last-name input")
     const emailField = document.querySelector(".email input")
     const queryField = document.querySelectorAll(".radio-block input")
-    const msgField = document.querySelector(".message input")
+    const msgField = document.querySelector(".message textarea")
     const consentField = document.querySelector(".consent-block input")
 
     if(fnField.value === ''){
@@ -53,6 +59,8 @@ function formValidator(item){
 
         fnSpan.classList.remove('invisible')
         fnField.classList.add("invalid")
+
+        isValid = false
 
     }
 
@@ -63,6 +71,8 @@ function formValidator(item){
         lnSpan.classList.remove('invisible')
         lnField.classList.add("invalid")
 
+        isValid = false
+
     }
 
     if(emailField.value === '' || !/^[a-z0-9]{1,}@[a-z]{1,}$/.test(emailField.value)){
@@ -72,6 +82,67 @@ function formValidator(item){
         emailSpan.classList.remove('invisible')
         emailField.classList.add("invalid")
 
+        isValid = false
+
     }
 
+    isQuerySet = false
+
+    queryField.forEach(query => {
+        if(query.checked === true){
+            isQuerySet = true
+        }
+    })
+
+    if(!isQuerySet){
+
+        let querySpan = document.querySelector(".query-type span")
+
+        querySpan.classList.remove('invisible')
+
+        isValid = false
+
+    }
+
+    if(msgField.value === ''){
+
+        let msgSpan = document.querySelector(".message span")
+
+        msgSpan.classList.remove('invisible')
+        msgField.classList.add("invalid")
+
+        isValid = false
+
+    }
+
+    if(consentField.checked === false){
+
+        let consentSpan = document.querySelector(".consent-block span")
+
+        consentSpan.classList.remove('invisible')   
+
+        isValid = false
+
+    }
+
+    if(isValid){
+
+        let successBlock = document.querySelector('.success-block')
+
+        successBlock.classList.remove("invisible")
+
+    }
+
+}
+
+function removeWarning(item){
+    if(item.name === 'consent'){
+        item.parentElement.parentElement.querySelector("span").classList.add('invisible')
+        item.classList.remove("invalid")
+    }
+    else{
+        item.parentElement.querySelector("span").classList.add('invisible')
+        item.classList.remove("invalid")
+    }
+    
 }
